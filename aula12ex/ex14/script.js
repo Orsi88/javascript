f887ae1af910e31011263f8bd2a9b2075fcf0328
Júlia Martins Orsi <102994938+Orsi88@users.noreply.github.com>
2022-04-21 10:36:07 -0300

    atualizei

---
+++ aula12ex/ex14/script.js
@@ -0,0 +1,31 @@
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('img')
+var res = window.document.getElementById('res')
+var data = new Date()
+var hora = data.getHours()
+
+msg.innerHTML = `Agora são ${hora} horas!`
+
+
+
+if (hora >= 0 && hora <=12) {
+
+    img.innerHTML = '<img src="imagens/manha-img.png">'
+    document.body.style.background = '#FCB23E'
+    res.innerText = 'Bom Dia!'
+    
+
+} else if (hora >= 12 && hora <= 18) {
+
+    img.innerHTML = '<img src="imagens/tarde-img.png">'
+    document.body.style.background = '#F56244'
+    res.innerText = 'Boa Tarde!'
+
+} else {
+
+    img.innerHTML = '<img src="imagens/noite-img.png">'
+    document.body.style.background = '#752346'
+    res.innerText = 'Boa Noite!'
+}
+
+
